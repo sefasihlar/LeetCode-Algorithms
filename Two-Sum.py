791aff961b814aa3d4f94a21e29dde662d455027
ayyıldız@software <sihlarsefa7@gmail.com>
2022-10-06 14:30:35 +0300

    two-sum solved

---
+++ Two-Sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = dict()
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in hash:
+                return [hash[x],i]
+            hash[nums[i]] = i
+        return [-1,-1]
+        
+                
